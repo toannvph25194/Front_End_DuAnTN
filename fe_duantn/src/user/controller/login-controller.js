22089879ac9b963c754a384c2d57a7c32f619c5f
DESKTOP-4SFC0M1\HP <toannvph25194@fpt.edu.vn>
2024-04-10 14:18:52 +0700

    toan update lại login

--- fe_duantn/src/user/controller/login-controller.js
+++ fe_duantn/src/user/controller/login-controller.js
@@ -3,35 +3,58 @@ app.controller("registerController", function ($scope, $http, $window) {
   $scope.taikhoan = "";
   $scope.email = "";
   $scope.matkhau = "";
-  $scope.chucvu = 2;
+
+  $scope.taikhoanLoginValid = true;
+  $scope.matkhauLoginValid = true;
+
+  $scope.taikhoanRegisValid = true;
+  $scope.taikhoanRegisLeng = true;
+  $scope.taikhoanRegisForcus = true;
+  var taikhoanRegexp = /^[a-zA-Z0-9]+$/;
+  $scope.matkhauRegisValid = true;
+  $scope.matkhauRegisLeng = true;
+  $scope.emailRegisValid = true;
+  $scope.emailRegisForcus = true;
+  var emailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   $scope.authenticate = function () {
+
+    if (!$scope.taikhoan && !$scope.matkhau) {
+      $scope.taikhoanLoginValid = false;
+      $scope.matkhauLoginValid = false;
+      return;
+    } else if (!$scope.taikhoan) {
+      $scope.taikhoanLoginValid = false;
+      $scope.matkhauLoginValid = true;
+      return;
+    } else if (!$scope.matkhau) {
+      $scope.taikhoanLoginValid = true;
+      $scope.matkhauLoginValid = false;
+      return;
+    } else {
+      $scope.taikhoanLoginValid = true;
+      $scope.matkhauLoginValid = true;
+    }
+
     var authenticationData = {
       taikhoan: $scope.taikhoan,
       matkhau: $scope.matkhau,
     };
 
     // Gửi yêu cầu POST đến API localhost:8080/ban/hang/login
-    $http.post("http://localhost:8080/ban/hang/login", authenticationData)
+    $http.post("http://localhost:8080/api/auth/khachhang/dangnhapkhachhang", authenticationData)
       .then(function (response) {
         console.log("Đăng nhập thành công:", response.data);
-
-        // Lưu AccessToken vào localStorage
-        var accessToken = response.data.accessToken; // Thay đổi thành accessToken
-        localStorage.setItem("accessToken", accessToken); // Thay đổi thành accessToken
 
         //Lưu Id TaiKhoan Và localstorage
         var idtk = response.data.idtk;
         localStorage.setItem("idtk", idtk);
 
         var taikhoan = response.data.username;
-        localStorage.setItem("username", taikhoan);
-        //lưu quyen hang
-        var role = response.data.role;
-        localStorage.setItem("role", role);
+        localStorage.setItem("taikhoan", taikhoan);
+
         // Hiển thị thông báo SweetAlert2 khi đăng nhập thành công
-        // Kiểm tra vai trò và chuyển hướng đến trang phù hợp
-        if (role === "USER") {
+        if (taikhoan != null) {
           Swal.fire({
             title: "Success",
             text: "Đăng nhập thành công tài khoản user",
@@ -43,48 +66,43 @@ app.controller("registerController", function ($scope, $http, $window) {
           });
 
           // Lấy IdTK sau khi đăng nhập
-          var idtaikhoantimkiem = localStorage.getItem('idtk');
-          console.log("IdTKDangNhap :", idtaikhoantimkiem);
-          // Hàm tìm kiếm id giỏ hàng của người dùng khi đăng nhập
-          $scope.finByIdGioHang = function () {
+          // var idtaikhoantimkiem = localStorage.getItem('idtk');
+          // console.log("IdTKDangNhap :", idtaikhoantimkiem);
+          // // Hàm tìm kiếm id giỏ hàng của người dùng khi đăng nhập
+          // $scope.finByIdGioHang = function () {
 
-            $http.get('http://localhost:8080/api/gio-hang/tim-kiem/gio-hang?idtk=' + idtaikhoantimkiem)
-              .then(resp => {
-                $scope.timKiemIdGH = resp.data;
+          //   $http.get('http://localhost:8080/api/gio-hang/tim-kiem/gio-hang?idtk=' + idtaikhoantimkiem)
+          //     .then(resp => {
+          //       $scope.timKiemIdGH = resp.data;
 
-                if ($scope.timKiemIdGH && $scope.timKiemIdGH.id !== null && $scope.timKiemIdGH.id !== "") {
-                  localStorage.setItem("idgiohang", $scope.timKiemIdGH.id);
-                  console.log("Đã Tìm Thấy IdGH :", resp)
-                } else {
-                  console.log("K Tìm Thấy IdGH Hoặc Chưa Tạo !");
-                }
+          //       if ($scope.timKiemIdGH && $scope.timKiemIdGH.id !== null && $scope.timKiemIdGH.id !== "") {
+          //         localStorage.setItem("idgiohang", $scope.timKiemIdGH.id);
+          //         console.log("Đã Tìm Thấy IdGH :", resp)
+          //       } else {
+          //         console.log("K Tìm Thấy IdGH Hoặc Chưa Tạo !");
+          //       }
 
-              }).catch(error => {
-                console.log("Lỗi K Load DC IDGH !")
-              });
-          }
-          $scope.finByIdGioHang();
+          //     }).catch(error => {
+          //       console.log("Lỗi K Load DC IDGH !")
+          //     });
+          // }
+          // $scope.finByIdGioHang();
 
           // Tải lại trang sau 1.5 giây khi thông báo biến mất
           setTimeout(function () {
             $window.location.href = "/src/user/index_user.html";
           }, 1500);
 
-        } else if (role === "ADMIN") {
+        } else {
           Swal.fire({
-            title: "Success",
-            text: "Đăng nhập thành công tài khoản Admin",
-            icon: "success",
+            title: "Đăng nhập thất bại",
+            text: "Sai tài khoản hoặc mật khẩu. Hãy thử lại!",
+            icon: "error",
             position: "top-end",
             toast: true,
             showConfirmButton: false,
             timer: 1500,
           });
-
-          // Tải lại trang sau 1.5 giây khi thông báo biến mất
-          setTimeout(function () {
-            $window.location.href = "/index_Admin.html";
-          }, 1500);
         }
 
       })
@@ -95,7 +113,7 @@ app.controller("registerController", function ($scope, $http, $window) {
         // Hiển thị thông báo đăng nhập thất bại (bạn có thể sử dụng thư viện thông báo thay vì alert)
         Swal.fire({
           title: "Error",
-          text: "Đăng nhập thất bại, hãy thử lại",
+          text: "Lỗi Đăng Nhập!",
           icon: "error",
           position: "top-end",
           toast: true,
@@ -103,23 +121,80 @@ app.controller("registerController", function ($scope, $http, $window) {
           timer: 1500,
         });
       });
-
   };
 
 
 
+
   $scope.register = function () {
+
+    if (!$scope.taikhoan) {
+      $scope.taikhoanRegisValid = false;
+    } else if ($scope.taikhoan.length < 7 || $scope.taikhoan.length > 13) {
+      $scope.taikhoanRegisLeng = false;
+      $scope.taikhoanRegisValid = true;
+      $scope.taikhoanRegisForcus = true;
+      return;
+    } else if (!taikhoanRegexp.test($scope.taikhoan)){
+      $scope.taikhoanRegisForcus = false;
+      $scope.taikhoanRegisLeng = true;
+      $scope.taikhoanRegisValid = true;
+    } else {
+      $scope.taikhoanRegisLeng = true;
+      $scope.taikhoanRegisValid = true;
+      $scope.taikhoanRegisForcus = true;
+    }
+
+    if (!$scope.email) {
+      $scope.emailRegisValid = false;
+    } else if (!emailRegexp.test($scope.email)) {
+      $scope.emailRegisForcus = false;
+      $scope.emailRegisValid = true;
+      return;
+    } else {
+      $scope.emailRegisForcus = true;
+      $scope.emailRegisValid = true;
+    }
+
+    if (!$scope.matkhau) {
+      $scope.matkhauRegisValid = false;
+    } else if($scope.matkhau.length < 6 || $scope.matkhau.length > 15){
+      $scope.matkhauRegisValid = true;
+      $scope.matkhauRegisLeng = false;
+      return;
+    } else { 
+      $scope.matkhauRegisValid = true;
+      $scope.matkhauRegisLeng = true;
+    }
+
+    // Thông báo không được để trống
+    if (
+      !$scope.taikhoanRegisValid ||
+      !$scope.matkhauRegisValid ||
+      !$scope.emailRegisValid 
+    ) {
+      // Swal.fire({
+      //   title: "Warning",
+      //     text: "Vui lòng nhập đầy đủ thông tin !",
+      //     icon: "warning",
+      //     position: "top-end",
+      //     toast: true,
+      //     showConfirmButton: false,
+      //     timer: 1500,
+      // });
+      return;
+    }
+
+
     var registrationData = {
       taikhoan: $scope.taikhoan,
       email: $scope.email,
       matkhau: $scope.matkhau,
-      chucvu: $scope.chucvu,
-      // Thêm các trường thông tin cần thiết khác
     };
 
     // Gửi yêu cầu POST đến API localhost:8080/ban/hang/register
     $http
-      .post("http://localhost:8080/ban/hang/register", registrationData)
+      .post("http://localhost:8080/api/auth/khachhang/dangkykhachhang", registrationData)
       .then(function (response) {
         console.log("Đăng ký thành công:", response.data);
 
@@ -145,7 +220,7 @@ app.controller("registerController", function ($scope, $http, $window) {
         // Hiển thị thông báo đăng ký thất bại (bạn có thể sử dụng thư viện thông báo thay vì alert)
         Swal.fire({
           title: "Error",
-          text: "Đăng ký thất bại, hãy thử lại",
+          text: "Lỗi đăng kí !",
           icon: "error",
           position: "top-end",
           toast: true,
