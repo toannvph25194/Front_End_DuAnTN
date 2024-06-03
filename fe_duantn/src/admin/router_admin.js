1efd879ea743225059e51bc5d523360095630ce5
ChangXinh23 <trangnthph24914@fpt.edu.vn>
2024-06-03 22:43:22 +0700

    trang push code giao diện qly thuộc tính sản phẩm

--- fe_duantn/src/admin/router_admin.js
+++ fe_duantn/src/admin/router_admin.js
@@ -27,6 +27,39 @@ app.config(function ($routeProvider, $locationProvider) {
     .when("/mausac", {
       templateUrl: "pages/mausac.html",
     })
+    .when("/size", {
+      templateUrl: "pages/size.html",
+    })
+    .when("/chatlieu", {
+      templateUrl: "pages/chatlieu.html",
+    })
+    .when("/xuatxu", {
+      templateUrl: "pages/xuatxu.html",
+    })
+    .when("/thuonghieu", {
+      templateUrl: "pages/thuonghieu.html",
+    })
+    .when("/danhmuc", {
+      templateUrl: "pages/danhmuc.html",
+    })
+    .when("/updatechatlieu", {
+      templateUrl: "pages/updatechatlieu.html",
+    })
+    .when("/updatedanhmuc", {
+      templateUrl: "pages/updatedanhmuc.html",
+    })
+    .when("/updatemausac", {
+      templateUrl: "pages/updatemausac.html",
+    })
+    .when("/updatsize", {
+      templateUrl: "pages/updatesize.html",
+    })
+    .when("/updatethuonghieu", {
+      templateUrl: "pages/updatethuonghieu.html",
+    })
+    .when("/updatexuatxu", {
+      templateUrl: "pages/updatexuatxu.html",
+    })
     .otherwise({
       redirectTo: "/",
     });
